a83e9172ba4b239341f28c4ed030fa5f8e039c32
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-03-11 19:43:19 +0400

    Add project files.

---
+++ HelloPowerOfTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloPowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
